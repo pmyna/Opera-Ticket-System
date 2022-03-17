21f9940063b457d1daca1aec12938d423246e429
pmyna <miname630@gmail.com>
2022-03-17 18:47:01 +0100

    Login Form

--- webapp.py
+++ webapp.py
@@ -19,7 +19,7 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Erfolgreich registriert! \n Ihre Kundennummer für den Login ist 123', 'success') #Kundennummer-Anzeige mit DB verbinden
+        flash(f'Registrierung erfolgreich! \n Ihre Kundennummer für den Login ist 123', 'success') #Kundennummer-Anzeige mit DB verbinden
         return redirect(url_for('home'))
     return render_template('register.html', title='Registrierung', form=form)
 
@@ -30,7 +30,9 @@ def login():
     if form.validate_on_submit():
         if form.customer_ID.data == '123' and form.password.data == 'test1234':
             flash(f'Erfolgreich eingeloggt', 'success')
-            return redirect(url_for('plan'))
+            return redirect(url_for('home'))
+        else:
+            flash('Kundennummer oder Passwort falsch', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
